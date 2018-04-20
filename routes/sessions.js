23fa4f1002850e2855c944ccbe1b8f8646afdde0
denis-ok <strelkov.d.d@mail.ru>
2018-04-20 20:52:17 +0300

    update routes

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,5 +1,33 @@
+// import buildFormObj from '../lib/formObjectBuilder';
+// import { encrypt } from '../lib/secure';
+// import { User } from '../models';
+
 export default (router) => {
-  router.get('sessions', '/sessions', (ctx) => {
-    ctx.body = 'Sessions page';
-  });
+  router
+    .get('newSession', '/session/new', async (ctx) => {
+      // const data = {};
+      // ctx.render('sessions/new', { formObj: buildFormObj(data) });
+      ctx.render('sessions/new', { title: 'Login Page' });
+    });
+  // .post('session', '/session', async (ctx) => {
+  //   const { email, password } = ctx.request.body.form;
+  //   const user = await User.findOne({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user && user.passwordDigest === encrypt(password)) {
+  //     ctx.session.userId = user.id;
+  //     ctx.redirect(router.url('root'));
+  //     return;
+  //   }
+
+  //   ctx.flash.set('email or password were wrong');
+  //   ctx.render('sessions/new', { f: buildFormObj({ email }) });
+  // })
+  // .delete('session', '/session', (ctx) => {
+  //   ctx.session = {};
+  //   ctx.redirect(router.url('root'));
+  // });
 };
+
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,66 @@
+// import buildFormObj from '../lib/formObjectBuilder';
+// import { User } from '../models/user';
+// import _ from 'lodash';
+
 export default (router) => {
-  router.get('users', '/users', (ctx) => {
-    ctx.body = 'Users page';
-  });
+  router
+    .get('users', '/users', async (ctx) => {
+      ctx.render('users');
+    })
+    .get('newUser', '/users/new', async (ctx) => {
+      ctx.render('users/new', { title: 'Registration' });
+    });
 };
+
+// .get('users', '/users', async (ctx) => {
+//   const users = await User.findAll(); // get all users from db model
+//   ctx.render('users', { users });
+// })
+// .get('newUser', '/users/new', (ctx) => {
+//   const user = User.build();  // create empty user model object instance for db (no save)
+//   ctx.render('users/new', { f: buildFormObj(user) }); // pass object to
+// })
+// .post('users', '/users', async (ctx) => {
+//   const form = ctx.request.body.form;
+//   const user = User.build(form);
+//   try {
+//     await user.save();
+//     ctx.flash.set('User has been created');
+//     ctx.redirect(router.url('root'));
+//   } catch (e) {
+//     ctx.render('users/new', { f: buildFormObj(user, e) });
+//   }
+// });
+
+// const errors = [{
+//   message: 'First or Lastname length must use only Alphabet letters',
+//   type: 'Validation error',
+//   path: 'firstName',
+//   value: '',
+// },
+// {
+//   message: 'First or Lastname length must be from 2 to 16 letters',
+//   type: 'Validation error',
+//   path: 'firstName',
+//   value: '',
+// },
+// {
+//   message: 'First or Lastname length must use only Alphabet letters',
+//   type: 'Validation error',
+//   path: 'lastName',
+//   value: '',
+// },
+// {
+//   message: 'First or Lastname length must be from 2 to 16 letters',
+//   type: 'Validation error',
+//   path: 'lastName',
+//   value: '',
+// }];
+
+// const formObj = {
+//   name: 'form',
+//   object: { firstName: 'denisuka' },
+//   errors: _.groupBy(errors, 'path'),
+// };
+
+// console.log(_.groupBy(errors, 'path'));
